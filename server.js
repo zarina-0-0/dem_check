fc0cce80e1f84c0f7af5594a1c796015ae390634
zarina-0-0 <b.zarina_2601@mail.ru>
2023-05-09 17:12:35 +0300

    Add some code

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use(express.static(path.join(__dirname, 'static')))
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, 'index.html'))
 })
-app.listen(9000, function() {
-    console.log("Listening on port 9000");
+app.listen(10000, function() {
+    console.log("Listening on port 10000");
 });
